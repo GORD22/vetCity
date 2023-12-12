feeb13996b1c98220f373eb19547e54616783a18
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-12-13 03:45:32 +0400

    added cardiology page and fixed stomatology

--- src/js/components/surgery_how-performed-swiper.js
+++ src/js/components/surgery_how-performed-swiper.js
@@ -1,28 +1,43 @@
-import Swiper from "swiper";
-import {Navigation} from "swiper/modules"
+import Swiper from 'swiper'
+import { Navigation } from 'swiper/modules'
 
-const surgeryHowPerformedSwiper = new Swiper(".how-performed-section__swiper", {
-    enabled: false,
+const surgeryHowPerformedSwiper = new Swiper('.how-performed-section__swiper', {
+	loop: true,
+	slidesPerView: 'auto',
+	modules: [Navigation],
+	navigation: {
+		prevEl: '.about-section__btn-prev',
+		nextEl: '.about-section__btn-next',
+	},
+	on: {
+		slideChange: function () {
+			updateActiveBullet(this.realIndex)
+		},
+		init: function () {
+			updateActiveBullet(this.realIndex)
+		},
+	},
+})
 
-    loop: true,
-
-    slidesPerView: 'auto',
-    modules: [Navigation],
-    slidesPerView: 'auto',
-    navigation: {
-        prevEl: ".about-section__btn-prev",
-        nextEl: ".about-section__btn-next",
-    },
-});
+function updateActiveBullet(index) {
+	const bullets = document.querySelectorAll('.swiper-pagination-bullet')
+	bullets.forEach((bullet, i) => {
+		if (i === index) {
+			bullet.classList.add('swiper-pagination-bullet-active')
+		} else {
+			bullet.classList.remove('swiper-pagination-bullet-active')
+		}
+	})
+}
 
 if (window.innerWidth > 1016) {
-    surgeryHowPerformedSwiper?.enable()
+	surgeryHowPerformedSwiper?.enable()
 }
 
 window.addEventListener('resize', () => {
-    if (window.innerWidth > 1016) {
-        surgeryHowPerformedSwiper?.enable()
-    } else {
-        surgeryHowPerformedSwiper.enabled = false
-    }
+	if (window.innerWidth > 1016) {
+		surgeryHowPerformedSwiper?.enable()
+	} else {
+		surgeryHowPerformedSwiper.enabled = false
+	}
 })
